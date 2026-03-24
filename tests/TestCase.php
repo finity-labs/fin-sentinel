483639d832f4ed4d2eabe372219f6a1fdfd58f81
Finity Labs <filament.infinity@gmail.com>
2026-03-24 19:39:12 +0100

    chore(07-01): set up test infrastructure with settings migrations and config defaults
    - Update TestCase with setUp loading settings migrations
    - Add Spatie settings service provider to package providers
    - Set config defaults for mail, scrub params/headers/env/trace_args
    - Add phpunit.xml.dist matching fin-mail structure
    - Create tests/Unit directory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,12 +6,21 @@ namespace FinityLabs\FinSentinel\Tests;
 
 use FinityLabs\FinSentinel\FinSentinelServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Spatie\LaravelSettings\LaravelSettingsServiceProvider;
 
 class TestCase extends Orchestra
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/settings');
+    }
+
     protected function getPackageProviders($app): array
     {
         return [
+            LaravelSettingsServiceProvider::class,
             FinSentinelServiceProvider::class,
         ];
     }
@@ -24,5 +33,12 @@ class TestCase extends Orchestra
             'database' => ':memory:',
             'prefix' => '',
         ]);
+
+        $app['config']->set('mail.default', 'array');
+
+        $app['config']->set('fin-sentinel.scrub.params', ['password', 'secret', 'token']);
+        $app['config']->set('fin-sentinel.scrub.headers', ['authorization']);
+        $app['config']->set('fin-sentinel.scrub.env', ['DB_PASSWORD']);
+        $app['config']->set('fin-sentinel.scrub.trace_args', ['password']);
     }
 }
